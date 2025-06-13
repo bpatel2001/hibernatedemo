45aaa9da4ebfe03595c2d1e8afd73f104b3f4f0b
bpatel2001 <125521942+bpatel2001@users.noreply.github.com>
2025-06-13 12:09:54 -0500

    Saved the student object into our database using a createStudent method that we created.

--- src/main/java/com/bpatel2001/hibernatedemo/HibernatedemoApplication.java
+++ src/main/java/com/bpatel2001/hibernatedemo/HibernatedemoApplication.java
@@ -1,5 +1,7 @@
 package com.bpatel2001.hibernatedemo;
 
+import com.bpatel2001.hibernatedemo.dao.StudentDAO;
+import com.bpatel2001.hibernatedemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,10 +16,26 @@ public class HibernatedemoApplication {
 
 	@Bean
 //	Will be executed after the Spring Beans have been loaded
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 //		Java Lambda Expression
 		return runner -> {
-			System.out.println("Hello World!");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+//		Create the student object
+
+		System.out.println("Creating new student object...");
+		Student tempStudent = new Student("Paul", "Doe", "paul@gmail.com");
+
+//		Save the student object
+
+		System.out.println("Saving the student...");
+		studentDAO.save(tempStudent);
+
+//		Display ID of the saved student
+
+		System.out.println("Saved student. Generated ID: " + tempStudent.getId());
 	}
 }
